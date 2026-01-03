98abe803d1abf0f0fc62fff8b5092708968cdc89
limdaecheol <eocjf852963@gmail.com>
2026-01-03 16:38:59 +0900

    initial commit

---
+++ src/main/java/com/example/ChungangLions/ChungangLionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChungangLions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChungangLionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChungangLionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ChungangLions/ChungangLionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChungangLions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChungangLionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
